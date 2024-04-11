4089fc3235585c813dd0b519d99a69e240e58544
NhaChan <chanb2014731@student.ctu.edu.vn>
2024-04-11 22:00:33 +0700

    Update Delete bug

--- src/components/AdminUser/AdminUser.jsx
+++ src/components/AdminUser/AdminUser.jsx
@@ -1,16 +1,29 @@
-import { Form } from 'antd'
+import { Button, Form } from 'antd'
 import React from 'react'
-import { WrapperHeader} from './style'
+import { WrapperHeader, WrapperUploadFile } from './style'
 import TableComponent from '../TableComponent/TableComponent'
+import InputComponent from '../InputComponent/InputComponent'
+import DrawerComponent from '../DrawerComponent/DrawerComponent'
+import Loading from '../LoadingComponent/Loading'
+import ModalComponent from '../ModalComponent/ModalComponent'
+import { getBase64 } from '../../utils'
 import { useEffect } from 'react'
+import * as message from '../../components/Message/Message'
 import { useState } from 'react'
 import { useSelector } from 'react-redux'
+import { useMutationHooks } from '../../hooks/useMutationHook'
 import * as UserService from '../../services/UserService'
-import { useQueryClient } from '@tanstack/react-query'
+import { useIsFetching, useQueryClient } from '@tanstack/react-query'
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons'
+
 
 const AdminUser = () => {
+  const [rowSelected, setRowSelected] = useState('')
+  const [isOpenDrawer, setIsOpenDrawer] = useState(false)
+  const [isLoadingUpdate, setIsLoadingUpdate] = useState(false)
+  const [isModalOpenDelete, setIsModalOpenDelete] = useState(false)
   const user = useSelector((state) => state?.user)
-  
+
   const [stateUserDetails, setStateUserDetails] = useState({
     name: '',
     email: '',
@@ -20,6 +33,48 @@ const AdminUser = () => {
   })
 
   const [form] = Form.useForm();
+  const mutationUpdate = useMutationHooks(
+    (data) => {
+      const { id,
+        token,
+        ...rests } = data
+      const res = UserService.updateUser(
+        id,
+        { ...rests }, token)
+      return res
+    },
+  )
+
+  const mutationDeletedMany = useMutationHooks(
+    (data) => {
+      const { token, ...ids
+      } = data
+      const res = UserService.deleteManyUser(
+        ids,
+        token)
+      return res
+    },
+  )
+
+  const handleDelteManyUsers = (ids) => {
+    mutationDeletedMany.mutate({ ids: ids, token: user?.access_token }, {
+      onSettled: () => {
+        queryClient.invalidateQueries(['users'])
+      }
+    })
+  }
+
+  const mutationDeleted = useMutationHooks(
+    (data) => {
+      const { id,
+        token,
+      } = data
+      const res = UserService.deleteUser(
+        id,
+        token)
+      return res
+    },
+  )
 
   const fetchGetDetailsUser = async (rowSelected) => {
     const res = await UserService.getDetailsUser(rowSelected)
@@ -32,14 +87,42 @@ const AdminUser = () => {
         avatar: res.data?.avatar
       })
     }
+    setIsLoadingUpdate(false)
   }
 
   useEffect(() => {
     form.setFieldsValue(stateUserDetails)
   }, [form, stateUserDetails])
 
+  useEffect(() => {
+    if (rowSelected && isOpenDrawer) {
+      setIsLoadingUpdate(true)
+      fetchGetDetailsUser(rowSelected)
+    }
+  }, [rowSelected, isOpenDrawer])
+
+  const handleDetailsProduct = () => {
+    setIsOpenDrawer(true)
+  }
+
+
+  const { data: dataUpdated, isPending: isLoadingUpdated, isSuccess: isSuccessUpdated, isError: isErrorUpdated } = mutationUpdate
+  const { data: dataDeleted, isPending: isLoadingDeleted, isSuccess: isSuccessDelected, isError: isErrorDeleted } = mutationDeleted
+  const { data: dataDeletedMany, isPending: isLoadingDeletedMany, isSuccess: isSuccessDelectedMany, isError: isErrorDeletedMany } = mutationDeletedMany
+
+
   const queryClient = useQueryClient()
   const users = queryClient.getQueryData(['users'])
+  const isFetchingUser = useIsFetching(['users'])
+
+  const renderAction = () => {
+    return (
+      <div>
+        <DeleteOutlined style={{ color: 'red', fontSize: '20px', cursor: 'pointer', padding: '5px' }} onClick={() => setIsModalOpenDelete(true)} />
+        <EditOutlined style={{ color: 'orange', fontSize: '20px', cursor: 'pointer', padding: '5px' }} onClick={handleDetailsProduct} />
+      </div>
+    )
+  }
 
   const columns = [
     {
@@ -61,20 +144,174 @@ const AdminUser = () => {
     {
       title: 'Action',
       dataIndex: 'action',
+      render: renderAction
     },
   ];
   const dataTable = users?.data?.length > 0 && users?.data
-  .filter(user => !user.isAdmin) // Lọc ra những người dùng không phải là admin
-  .map(user => ({ ...user, key: user._id }));
+    .filter(user => !user.isAdmin) // Lọc ra những người dùng không phải là admin
+    .map(user => ({ ...user, key: user._id }));
+
+  useEffect(() => {
+    if (isSuccessDelected && dataDeleted?.status === 'OK') {
+      message.success()
+      handleCancelDelete()
+    } else if (isErrorDeleted) {
+      message.error()
+    }
+  }, [isSuccessDelected])
+
+  useEffect(() => {
+    if (isSuccessDelectedMany && dataDeletedMany?.status === 'OK') {
+      message.success()
+    } else if (isErrorDeletedMany) {
+      message.error()
+    }
+  }, [isSuccessDelectedMany])
+
+  const handleCloseDrawer = () => {
+    setIsOpenDrawer(false);
+    setStateUserDetails({
+      name: '',
+      email: '',
+      phone: '',
+      isAdmin: false,
+    })
+    form.resetFields()
+  };
+
+  useEffect(() => {
+    if (isSuccessUpdated && dataUpdated?.status === 'OK') {
+      message.success()
+      handleCloseDrawer()
+    } else if (isErrorUpdated) {
+      message.error()
+    }
+  }, [isSuccessUpdated])
+
+  const handleCancelDelete = () => {
+    setIsModalOpenDelete(false)
+  }
+
+  const handleDeleteUser = () => {
+    mutationDeleted.mutate({ id: rowSelected, token: user?.access_token }, {
+      onSettled: () => {
+        queryClient.invalidateQueries(['users'])
+      }
+    })
+  }
+
+  const handleOnchangeDetails = (e) => {
+    setStateUserDetails({
+      ...stateUserDetails,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleOnchangeAvatarDetails = async ({ fileList }) => {
+    const file = fileList[0]
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj);
+    }
+    setStateUserDetails({
+      ...stateUserDetails,
+      avatar: file.preview
+    })
+  }
+
+  const onUpdateUser = () => {
+    mutationUpdate.mutate({ id: rowSelected, token: user?.access_token, ...stateUserDetails }, {
+      onSettled: () => {
+        queryClient.invalidateQueries(['users'])
+      }
+    })
+  }
 
   return (
     <div>
       <WrapperHeader>Quản lý người dùng</WrapperHeader>
       <div style={{ marginTop: '20px' }}>
-        <TableComponent  columns={columns} data={dataTable} onRow={(record, rowIndex) => {
-
+        <TableComponent handleDelteMany={handleDelteManyUsers} columns={columns} data={dataTable} onRow={(record) => {
+          return {
+            onClick: event => {
+              setRowSelected(record._id)
+            }
+          };
         }} />
       </div>
+
+      <DrawerComponent title='Chi tiết người dùng' isOpen={isOpenDrawer} onClose={() => setIsOpenDrawer(false)} width="60%">
+        <Loading isPending={isLoadingUpdate || isLoadingUpdated}>
+
+          <Form
+            name="basic"
+            labelCol={{ span: 2 }}
+            wrapperCol={{ span: 22 }}
+            onFinish={onUpdateUser}
+            autoComplete="on"
+            form={form}
+          >
+            <Form.Item
+              label="Tên"
+              name="name"
+              rules={[{ required: true, message: 'Please input your name!' }]}
+            >
+              <InputComponent value={stateUserDetails['name']} onChange={handleOnchangeDetails} name="name" />
+            </Form.Item>
+
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[{ required: true, message: 'Please input your email!' }]}
+            >
+              <InputComponent value={stateUserDetails['email']} onChange={handleOnchangeDetails} name="email" />
+            </Form.Item>
+            <Form.Item
+              label="Điện thoại"
+              name="phone"
+              rules={[{ required: true, message: 'Please input your  phone!' }]}
+            >
+              <InputComponent value={stateUserDetails.phone} onChange={handleOnchangeDetails} name="phone" />
+            </Form.Item>
+
+            <Form.Item
+              label="Địa chỉ"
+              name="address"
+              rules={[{ required: true, message: 'Please input your  address!' }]}
+            >
+              <InputComponent value={stateUserDetails.address} onChange={handleOnchangeDetails} name="address" />
+            </Form.Item>
+
+            <Form.Item
+              label="Ảnh"
+              name="avatar"
+              rules={[{ required: true, message: 'Please input your image!' }]}
+            >
+              <WrapperUploadFile onChange={handleOnchangeAvatarDetails} maxCount={1}>
+                <Button >Select File</Button>
+                {stateUserDetails?.avatar && (
+                  <img src={stateUserDetails?.avatar} style={{
+                    height: '60px',
+                    width: '60px',
+                    borderRadius: '50%',
+                    objectFit: 'cover',
+                    marginLeft: '10px'
+                  }} alt="avatar" />
+                )}
+              </WrapperUploadFile>
+            </Form.Item>
+            <Form.Item wrapperCol={{ offset: 20, span: 16 }}>
+              <Button type="primary" htmlType="submit">
+                Apply
+              </Button>
+            </Form.Item>
+          </Form>
+        </Loading>
+      </DrawerComponent>
+      <ModalComponent title="Xóa người dùng" open={isModalOpenDelete} onCancel={handleCancelDelete} onOk={handleDeleteUser}>
+        <Loading isPending={isLoadingDeleted}>
+          <div>Bạn có chắc xóa tài khoản này không?</div>
+        </Loading>
+      </ModalComponent>
     </div>
   )
 }
